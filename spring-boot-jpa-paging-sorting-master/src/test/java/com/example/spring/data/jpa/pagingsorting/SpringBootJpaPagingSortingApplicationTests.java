499837ebd1c2c71e164c6b919d79babfcf466d57
Vishwa <vishurudra@yahoo.co.in>
2023-11-25 20:04:25 +0530

    sorting

---
+++ spring-boot-jpa-paging-sorting-master/src/test/java/com/example/spring/data/jpa/pagingsorting/SpringBootJpaPagingSortingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring.data.jpa.pagingsorting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJpaPagingSortingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
